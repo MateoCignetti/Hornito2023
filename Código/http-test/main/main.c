3d3ba51a2a25073c3ebb17fb283d33c1588d296b
MateoCignetti <mateocig18@gmail.com>
2023-10-02 23:29:03 -0300

    Implementación de tabla con agregado de filas por html

--- Código/http-test/main/main.c
+++ Código/http-test/main/main.c
@@ -14,9 +14,9 @@
 
 // Main
 void app_main(void){
-    setup_time();
+    //setup_time();
     setup_wifi();
-    create_time_task();
+    //create_time_task();
     httpd_handle_t webserver = start_webserver();
 }
 //
--- Código/http-test/main/webserver.c
+++ Código/http-test/main/webserver.c
@@ -1,10 +1,12 @@
 #include "webserver.h"
+#include "cJSON.h"
 
 #define HTML_NEG_OFFSET 1
 
 // Private function prototypes
 static esp_err_t root_get_handler(httpd_req_t *req);
 static esp_err_t time_post_handler(httpd_req_t *req);
+static esp_err_t obtener_datos_handler(httpd_req_t *req);
 //
 
 // Global variables
@@ -19,16 +21,24 @@ static const httpd_uri_t time_post = {
     .method = HTTP_POST,
     .handler = time_post_handler,
 };
+
+static const httpd_uri_t obtener_datos = {
+    .uri = "/obtener_datos",
+    .method = HTTP_GET,
+    .handler = obtener_datos_handler,
+};
 //
 
 // Functions
 httpd_handle_t start_webserver(){
     httpd_handle_t httpd_server = NULL;
     httpd_config_t httpd_config = HTTPD_DEFAULT_CONFIG();
+    httpd_config.stack_size = 2*4096;
 
     if(httpd_start(&httpd_server, &httpd_config) == ESP_OK){
         httpd_register_uri_handler(httpd_server, &root);
         httpd_register_uri_handler(httpd_server, &time_post);
+        httpd_register_uri_handler(httpd_server, &obtener_datos);
         return httpd_server;
     } else{
         return NULL;
@@ -92,6 +102,38 @@ esp_err_t time_post_handler(httpd_req_t *req)
     httpd_resp_set_status(req, "303 See Other");
     httpd_resp_set_hdr(req, "Location", "/");
     httpd_resp_send(req, NULL, 0);
+
+    return ESP_OK;
+}
+
+esp_err_t obtener_datos_handler(httpd_req_t *req)
+{
+    // Simula datos de ejemplo (puedes cargar estos datos desde tu fuente real)
+    cJSON *datos = cJSON_CreateArray();
+    cJSON *dato1 = cJSON_CreateObject();
+    cJSON_AddStringToObject(dato1, "tiempo", "12:30:00");
+    cJSON_AddStringToObject(dato1, "potencia", "120");
+    cJSON_AddStringToObject(dato1, "temperaturaPF", "12°C");
+    cJSON_AddStringToObject(dato1, "temperaturaPC", "58°C");
+    cJSON_AddItemToArray(datos, dato1);
+
+    cJSON *dato2 = cJSON_CreateObject();
+    cJSON_AddStringToObject(dato2, "tiempo", "12:35:00");
+    cJSON_AddStringToObject(dato2, "potencia", "130");
+    cJSON_AddStringToObject(dato2, "temperaturaPF", "13°C");
+    cJSON_AddStringToObject(dato2, "temperaturaPC", "59°C");
+    cJSON_AddItemToArray(datos, dato2);
+
+    // Genera una cadena JSON a partir de los datos
+    char *json_str = cJSON_PrintUnformatted(datos);
+
+    // Configura la respuesta HTTP con los datos JSON
+    httpd_resp_set_type(req, "application/json");
+    httpd_resp_send(req, json_str, strlen(json_str));
+
+    // Limpia la memoria
+    cJSON_Delete(datos);
+    free(json_str);
 
     return ESP_OK;
 }
