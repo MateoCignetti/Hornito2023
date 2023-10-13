b38186ff89a5626575fd8f8ca9151fb935b6202a
manu-rodr98 <manu_rodr@hotmail.com>
2023-10-13 18:53:01 -0300

    Agregar comentarios en http-test

--- Codigo/http-test/main/date_time.c
+++ Codigo/http-test/main/date_time.c
@@ -1,38 +1,38 @@
 #include "date_time.h"
 
 #define TIME_DELAY_MS 1000
-#define CUSTOM_TIME 1695687516
+#define CUSTOM_TIME 1695687516  //Custom time value to set the system time.
 
 void setup_time(){
-    setenv("TZ", "WART4WARST,J1/0,J365/25", 1); //Zona horaria "WART4WARST" (Hora de Argentina).
-    tzset();                                    // Actualiza las variables de entorno relacionadas con la zona horaria.
+    setenv("TZ", "WART4WARST,J1/0,J365/25", 1); //Timezone setting for "WART4WARST" (Argentina Time).
+    tzset();                                    //Update environment variables related to timezone.
 }
 
-//Task para sincronización del tiempo e impresión de fecha y hora.
+//Task for time synchronization and printing date and time.
 void time_task(){
-    time_t now;             //Tiempo actual en segundos desde el 1 de enero de 1970 (Unix epoch)
-    char strftime_buf[64];  //Búfer para almacenar la fecha y hora formateadas como una cadena de caracteres.
-    struct tm timeinfo;     //Estructura para almacenar la fecha y hora desglosadas
+    time_t now;             //Current time in seconds since January 1, 1970 (Unix epoch).
+    char strftime_buf[64];  //Buffer to store formatted date and time as a string.
+    struct tm timeinfo;     //Structure to store broken-down date and time.
 
-    struct timeval custom_tv = { .tv_sec = CUSTOM_TIME }; //Estructura timeval con el tiempo "CUSTOM_TIME".
-    settimeofday(&custom_tv, NULL);                       //Establece la hora actual usando la estructura timeval.
-    TickType_t xLastWakeTime = xTaskGetTickCount();       //Obtiene el tiempo de ejecución actual de la tarea.
+    struct timeval custom_tv = { .tv_sec = CUSTOM_TIME }; //Define timeval structure with "CUSTOM_TIME".
+    settimeofday(&custom_tv, NULL);                       //Set the current time using timeval structure.
+    TickType_t xLastWakeTime = xTaskGetTickCount();       //Get current task execution time.
 
     while(true){
-        time(&now);                                                         //Obtención del tiempo actual en segundos.
-        localtime_r(&now, &timeinfo);                                       //Convierte el tiempo actual en una estructura tm local.
-        strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);      //Formatea la estructura tm en una cadena de caracteres.
+        time(&now);                                                         //Get current time in seconds.
+        localtime_r(&now, &timeinfo);                                       //Convert current time to local tm structure.
+        strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);      //Format tm structure into a string.
         printf("The current date/time in Cordoba is: %s\n", strftime_buf);
-        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(TIME_DELAY_MS));
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(TIME_DELAY_MS));      //Delay until the next execution.
     }
 }
 
 void set_time(int time){
-    time_t now = time;                              
-    struct timeval custom_tv = { .tv_sec = time };  //Estructura timeval con el tiempo especificado.
-    settimeofday(&custom_tv, NULL);                 //Establece la hora actual usando la estructura timeval
+    time_t now = time;                              //Convert the time value to time_t type.
+    struct timeval custom_tv = { .tv_sec = time };  //Define timeval structure with specified time.
+    settimeofday(&custom_tv, NULL);                 //Set the current time using timeval structure.
 }
 
 void create_time_task(){
-    xTaskCreate(time_task, "time_task", 4096, NULL, 5, NULL);
+    xTaskCreate(time_task, "time_task", 4096, NULL, 5, NULL);   //Create the "time_task" task with a stack size of 4096 words and priority 5.
 }
