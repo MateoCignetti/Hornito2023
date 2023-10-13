ef27e1fc3276d876ee24389ae08dd21c6379a3c5
manu-rodr98 <manu_rodr@hotmail.com>
2023-10-13 18:36:17 -0300

    Agregar comentarios en http-test

--- Codigo/http-test/main/date_time.c
+++ Codigo/http-test/main/date_time.c
@@ -4,33 +4,33 @@
 #define CUSTOM_TIME 1695687516
 
 void setup_time(){
-    setenv("TZ", "WART4WARST,J1/0,J365/25", 1);
-    tzset();
+    setenv("TZ", "WART4WARST,J1/0,J365/25", 1); //Zona horaria "WART4WARST" (Hora de Argentina).
+    tzset();                                    // Actualiza las variables de entorno relacionadas con la zona horaria.
 }
 
+//Task para sincronización del tiempo e impresión de fecha y hora.
 void time_task(){
-    time_t now;
-    char strftime_buf[64];
-    struct tm timeinfo;
+    time_t now;             //Tiempo actual en segundos desde el 1 de enero de 1970 (Unix epoch)
+    char strftime_buf[64];  //Búfer para almacenar la fecha y hora formateadas como una cadena de caracteres.
+    struct tm timeinfo;     //Estructura para almacenar la fecha y hora desglosadas
 
-    struct timeval custom_tv = { .tv_sec = CUSTOM_TIME };
-    settimeofday(&custom_tv, NULL);
-    TickType_t xLastWakeTime = xTaskGetTickCount();
+    struct timeval custom_tv = { .tv_sec = CUSTOM_TIME }; //Estructura timeval con el tiempo "CUSTOM_TIME".
+    settimeofday(&custom_tv, NULL);                       //Establece la hora actual usando la estructura timeval.
+    TickType_t xLastWakeTime = xTaskGetTickCount();       //Obtiene el tiempo de ejecución actual de la tarea.
 
     while(true){
-        
-        time(&now);
-        localtime_r(&now, &timeinfo);
-        strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+        time(&now);                                                         //Obtención del tiempo actual en segundos.
+        localtime_r(&now, &timeinfo);                                       //Convierte el tiempo actual en una estructura tm local.
+        strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);      //Formatea la estructura tm en una cadena de caracteres.
         printf("The current date/time in Cordoba is: %s\n", strftime_buf);
         vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(TIME_DELAY_MS));
     }
 }
 
 void set_time(int time){
-    time_t now = time;
-    struct timeval custom_tv = { .tv_sec = time };
-    settimeofday(&custom_tv, NULL);
+    time_t now = time;                              
+    struct timeval custom_tv = { .tv_sec = time };  //Estructura timeval con el tiempo especificado.
+    settimeofday(&custom_tv, NULL);                 //Establece la hora actual usando la estructura timeval
 }
 
 void create_time_task(){
--- Codigo/http-test/main/wifi.c
+++ Codigo/http-test/main/wifi.c
@@ -1,8 +1,8 @@
 #include "wifi.h"
 
 // Defines
-#define AP_SSID "Hornito"
-#define AP_PASSWORD "noseashater"
+#define AP_SSID     "Hornito"       //Nombre de la red Wi-Fi (SSID)
+#define AP_PASSWORD "noseashater"   //Contraseña de la red Wi-Fi
 //
 
 // Private function prototypes
@@ -17,31 +17,31 @@ void setup_wifi(){
     wifi_config();
     wifi_start();
 }
-
+//Inicialización de la pila de red LwIP y el controlador Wi-Fi
 static void wifi_lwip_init(){
-    nvs_flash_init();
-    esp_netif_init();
-    esp_event_loop_create_default();
-    esp_netif_create_default_wifi_ap();
-    wifi_init_config_t wifi_config = WIFI_INIT_CONFIG_DEFAULT();
-    wifi_config.nvs_enable = 0;
-    esp_wifi_init(&wifi_config);
+    nvs_flash_init();                       //Inicializa la memoria flash no volátil (NVS) para el almacenamiento de configuración
+    esp_netif_init();                       //Inicializa la interfaz de red ESP-IDF
+    esp_event_loop_create_default();        //Crea un bucle de eventos predeterminado para manejar eventos de red
+    esp_netif_create_default_wifi_ap();     //Crea una interfaz Wi-Fi en modo punto de acceso (AP)
+    wifi_init_config_t wifi_config = WIFI_INIT_CONFIG_DEFAULT(); //Configuración predeterminada de Wi-Fi
+    wifi_config.nvs_enable = 0;             //Deshabilita el uso de NVS para la configuración Wi-Fi
+    esp_wifi_init(&wifi_config);            //Inicializa el controlador Wi-Fi con la configuración proporcionada
 }
-
+//Configura la conexión Wi-Fi en modo AP con el SSID y la contraseña especificados
 static void wifi_config(){
-    esp_wifi_set_mode(WIFI_MODE_AP);
+    esp_wifi_set_mode(WIFI_MODE_AP);        //Configura el modo de Wi-Fi como punto de acceso (AP)
 
     wifi_config_t wifi_config = {
         .ap = {
-            .ssid = AP_SSID,
-            .password = AP_PASSWORD,
-            .ssid_len = strlen(AP_SSID),
-            .authmode = WIFI_AUTH_WPA2_PSK,
-            .max_connection = 10,
+            .ssid = AP_SSID,                //Establece el nombre de la red Wi-Fi (SSID)
+            .password = AP_PASSWORD,        //Establece la contraseña de la red Wi-Fi
+            .ssid_len = strlen(AP_SSID),    //Calcula la longitud del SSID
+            .authmode = WIFI_AUTH_WPA2_PSK, //Establece el modo de autenticación (WPA2-PSK)
+            .max_connection = 10,           //Establece el número máximo de clientes permitidos
         }
     };
 
-    esp_wifi_set_config(WIFI_IF_AP, &wifi_config);
+    esp_wifi_set_config(WIFI_IF_AP, &wifi_config);  //Configura los parámetros de la red AP con la configuración proporcionada
 }
 
 static void wifi_start(){
